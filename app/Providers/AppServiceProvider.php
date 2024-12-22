242b80ada230f4f44df6d92c9c23209267d81d35
HakimFIDJEL <hakim.fidjel@gmail.com>
2024-12-22 01:06:29 +0100

    22/12 forgot import ^^

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        
+
 
         // Force HTTPS in production
         if ($this->app->environment('production')) {
