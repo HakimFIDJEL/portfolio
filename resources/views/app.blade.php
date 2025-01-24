3ff94041a3f8f1cb0b4858f4cbb113c7282cdec8
HakimFIDJEL <hakimfidjel.pro@gmail.com>
2025-01-24 14:23:23 +0100

    referencement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta name="og:title" content="Hakim - My portfolio">
+    <meta name="og:site-name" content="hakimfidjel.fr">
+    <meta name="og:description" content="Portfolio of Hakim Fidjel, a FullStack engineering apprentice. Discover my projects, skills, and ambitions across the diverse fields of computer engineering.">
+    <meta name="og:type" content="website">
+    <meta name="og:url" content="https://hakimfidjel.fr">
+    <meta name="og:image" content="https://hakimfidjel.fr/favicon.webp">
+
     {{-- Favicon --}}
     <link rel="icon" href="{{ asset('favicon.webp') }}" type="image/x-icon" />
+    
 
     @viteReactRefresh
 
