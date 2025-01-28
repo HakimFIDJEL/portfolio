942c5d4b39786ce3ef732e194013d564898f6c04
HakimFIDJEL <hakimfidjel.pro@gmail.com>
2025-01-28 10:41:05 +0100

    new sitemap

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -20,7 +20,7 @@ class GenerateSitemap extends Command
         $sitemap->add(
             Url::create('/')
                 ->setPriority(1.0)
-                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setChangeFrequency('daily')
         );
 
         // Ajouter les projets
@@ -29,7 +29,7 @@ class GenerateSitemap extends Command
             $sitemap->add(
                 Url::create('/project/' . $project->slug . '/' . $project->id)
                     ->setPriority(0.9)
-                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                    ->setChangeFrequency('weekly')
             );
         }
 
@@ -39,44 +39,13 @@ class GenerateSitemap extends Command
             $sitemap->add(
                 Url::create('/project/' . $lab->slug . '/' . $lab->id)
                     ->setPriority(0.8)
-                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                    ->setChangeFrequency('weekly')
             );
         }
 
-        // Personnaliser le rendu du sitemap pour inclure <priority> et <changefreq>
-        $sitemapXml = $this->renderWithPriority($sitemap);
-
-        // Écrire la sitemap personnalisée dans un fichier
-        file_put_contents(public_path('sitemap.xml'), $sitemapXml);
+        // Enregistrer le sitemap
+        $sitemap->writeToFile(public_path('sitemap.xml'));
 
         $this->info('Sitemap generated successfully.');
-    }
-
-    // Méthode pour inclure explicitement <priority> et <changefreq> dans le XML généré
-    private function renderWithPriority(Sitemap $sitemap): string
-    {
-        $xml = $sitemap->render();
-
-        // Ajouter les balises <priority> et <changefreq> manuellement
-        $xml = preg_replace_callback('/<url>(.*?)<\/url>/s', function ($matches) {
-            $urlBlock = $matches[1];
-
-            // Ajouter <priority> et <changefreq> si manquants
-            $priority = '1.0'; // Définir une priorité par défaut
-            $changefreq = 'daily'; // Définir une fréquence de changement par défaut
-
-            // Vérifier la priorité et la fréquence des pages
-            if (strpos($urlBlock, '/project/') !== false) {
-                $priority = '0.9';
-                $changefreq = 'weekly';
-            } elseif (strpos($urlBlock, '/lab/') !== false) {
-                $priority = '0.8';
-                $changefreq = 'weekly';
-            }
-
-            return "<url>{$urlBlock}<priority>{$priority}</priority><changefreq>{$changefreq}</changefreq></url>";
-        }, $xml);
-
-        return $xml;
     }
 }
