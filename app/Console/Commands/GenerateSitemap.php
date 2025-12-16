8670b5005c122498aba6f35f77a04b1180562f1d
Hakim <hakimfidjel.pro@gmail.com>
2025-12-16 10:07:13 +0100

    [FEAT] - Sitemap generation

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use Illuminate\Support\Facades\App;
+
+// Models
+use App\Models\Backoffice\Project;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the website sitemap';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Starting sitemap generation...');
+
+        $sitemap = Sitemap::create(config('app.url'));
+
+        $sitemap->add(
+            Url::create(route('home', [], absolute: true))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(1.0)
+        );
+
+        $sitemap->add(
+            Url::create(route('terms', [], absolute: true))
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
+                ->setPriority(0.5)
+        );
+
+        $projects = Project::all();
+
+        foreach ($projects as $project) {
+            $slugFr = $project->slug_fr;
+            $slugEn = $project->slug_en;
+            $routeFr = $slugFr ? route('project', ['slug' => $slugFr], absolute: true) : null;
+            $routeEn = $slugEn ? route('project', ['slug' => $slugEn], absolute: true) : null;
+            $type = $project->type;
+            $lastModified = $project->updated_at ?? now();
+
+            if ($routeFr) {
+                $frTag = Url::create($routeFr)
+                    ->setLastModificationDate($lastModified)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                    ->setPriority($type === 'project' ? 0.8 : 0.6);
+
+                if($routeEn) {
+                    $frTag->addAlternate($routeEn, 'en');
+                }
+
+                $sitemap->add($frTag);
+            }
+
+            if ($routeEn) {
+                $enTag = Url::create($routeEn)
+                    ->setLastModificationDate($lastModified)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                    ->setPriority($type === 'project' ? 0.8 : 0.6);
+
+                if($routeFr) {
+                    $enTag->addAlternate($routeFr, 'fr');
+                }
+
+                $sitemap->add($enTag);
+            }
+        }
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generated successfully at: ' . public_path('sitemap.xml'));
+
+        return Command::SUCCESS;
+    }
+}
