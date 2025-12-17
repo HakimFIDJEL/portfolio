8af4b1fdec1c04b4ab9830f7b22966f7fce15a5b
Hakim <hakimfidjel.pro@gmail.com>
2025-12-17 09:57:14 +0100

    [HOTFIX] - Errors translation

--- lang/fr/common.php
+++ lang/fr/common.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Flash messages
+    | Messages flash
     |--------------------------------------------------------------------------
     |
     */
@@ -15,10 +15,63 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Pages content
+    | Contenu des pages
     |--------------------------------------------------------------------------
     |
     */
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autres
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'themes' => [
+        'light' => 'Clair',
+        'dark' => 'Sombre',
+        'system' => 'Système',
+    ],
+
+    'colors' => [
+        'default' => 'Par défaut',
+        'blue' => 'Bleu',
+        'red' => 'Rouge',
+        'green' => 'Vert',
+        'orange' => 'Orange',
+        'rose' => 'Rose',
+        'violet' => 'Violet',
+        'yellow' => 'Jaune',
+    ],
+
+    'errors' => [
+        'button_back' => 'Retour à l\'accueil',
+
+        'default' => [
+            'title' => 'Une erreur est survenue',
+            'message' => 'Une erreur inattendue est survenue.',
+        ],
+        '403' => [
+            'title' => 'Accès refusé',
+            'message' => 'Vous n\'avez pas la permission d\'accéder à cette page.',
+        ],
+        '404' => [
+            'title' => 'Page non trouvée',
+            'message' => 'La page que vous recherchez est introuvable.',
+        ],
+        '500' => [
+            'title' => 'Erreur serveur',
+            'message' => 'Une erreur inattendue est survenue sur le serveur.',
+        ],
+        '501' => [
+            'title' => 'Non implémenté',
+            'message' => 'La fonctionnalité demandée n\'est pas implémentée.',
+        ],
+        '503' => [
+            'title' => 'Service indisponible',
+            'message' => 'Le service est actuellement indisponible. Veuillez réessayer plus tard.',
+        ],
+    ]
 
 ];
